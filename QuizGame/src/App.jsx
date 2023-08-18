2551cf789161802243980d5817fc84210436395d
Andres <andresgilbe2021@gmail.com>
2023-08-18 14:26:20 -0400

    Adding questions and answers and some details.

--- QuizGame/src/App.jsx
+++ QuizGame/src/App.jsx
@@ -1,23 +1,25 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
+
 const ProgressBar = ({ percentage }) => {
   return (
     <div className="progress-bar-container">
       <div className="progress-bar" style={{ width: `${percentage}%` }}>
-        {percentage > 0 && <span>{`${percentage}%`}</span>}
+        {percentage > 0 && <span className='progressbar'>{`${percentage}%`}</span>}
       </div>
     </div>
   );
 };
 
 
+
+
+
 function App() {
 
   const [count, setCount] = useState(0)
-  const test = Math.floor(Math.random() * 5); 
-
-  
+  const randomnumber = Math.floor(Math.random() * 5); 
   const [progress, setProgress] = useState(0);
 
   const increaseProgress = () => {
@@ -32,29 +34,32 @@ function App() {
     }
   };
 
-
-
   return (
     
   <div className='main app'>
-
-
-
       <ProgressBar percentage={progress} />
-
-
-
-    
     <div className='bar'> </div>
         <div className='progress'>Quiz Progress | {count} | </div>
+
+        <h3>Que edad tienes?</h3>
+    <div className='answers'>
+      <label className='box'>30</label>
+      <label className='box'>25</label>
+      <label className='box'>10</label>
+    </div>
+
 
         <div>
         <button onClick={increaseProgress}>Submit</button>
        <button onClick={numberQuestion}> NumberQuestion</button>
         </div>
+
+        
   </div>
   
   );
 }
 
 export default App
+
+
---
+++ QuizGame/src/assets/Questions.js
@@ -0,0 +1,31 @@
+/*
+const Preguntas = () => {
+  const dato
+}
+
+
+const Preguntas = () => {
+  const [dato,setDato] = useState([
+    {
+      pregunta: 'Como te llamas?',
+      respuesta: 'Carlos',
+      respuestas: 'Andres'
+    },
+    {
+      pregunta: 'Cual es tu edad?',
+      respuesta: '20',
+      respuesta: '21'
+    }
+  ]);
+  return(
+    <div>
+      {preguntas.map((pregunta) => {
+        return (
+          <div>
+            </div>
+        );
+      })}
+    </div>
+
+
+*/
